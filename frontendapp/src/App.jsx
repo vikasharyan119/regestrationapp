d3245bdc493e4089c9434918ce47c663c5f65bde
Vikas Kumar <your@email.com>
2025-06-16 20:32:55 +0530

    add in hashrouter

--- frontendapp/src/App.jsx
+++ frontendapp/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginSignupPage from './pages/LoginSignupPage';
 import Header from './components/Header';
